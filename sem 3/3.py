4ab744f2c53633cd9feea7f4c84140c8ad81b903
katerinaivanova77 <katerina_ivanova.05@mail.ru>
2023-09-26 08:55:49 +0300

    i

--- sem 3/3.py
+++ sem 3/3.py
@@ -1,1 +1,15 @@
-print(123)
+a = int(input())
+b = int(input())
+a1 = a
+b1 = b
+while a1 != 0 and b1 != 0:
+    if a1>b1:
+        a1 = a1 % b1
+    else:
+        b1 = b1 % a1
+d = a1 + b1
+for x in range(-500,500):
+    for y in range(-500,500):
+        if a*x + b*y == d:
+            print(x, y)
+print(d)
