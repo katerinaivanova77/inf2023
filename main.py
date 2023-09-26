446f2ae5470b79c6d4ac6dad033952fe1ae18a6f
katerinaivanova77 <katerina_ivanova.05@mail.ru>
2023-09-26 08:55:49 +0300

    i

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+size = int(input())
+symb = input()
+for i in range(1, size//2 + 1):
+    print(symb*i)
+if size%2 == 1:
+    print(symb * (size//2 + 1))
+for i in range(size//2, 0, -1):
+    print(symb*i)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
