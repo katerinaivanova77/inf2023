494fb167164b9467f9cf474b2cf0c5102d2e8511
katerinaivanova77 <katerina_ivanova.05@mail.ru>
2023-10-02 23:08:32 +0300

    i

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ iris_data = pd.read_csv("iris_data.csv")
 species_counts = iris_data["Species"].value_counts()
 plt.figure(figsize=(10, 10))
 plt.pie(species_counts, labels=species_counts.index)
-plt.title("Доля видов ирисов в датасете")
+plt.title("Виды ирисов в датасете")
 plt.show()
 
 plc = pd.cut(iris_data["PetalLengthCm"], bins=[0, 1.2, 1.5, np.inf], labels=["<= 1.2", "> 1.2 and <= 1.5", "> 1.5"])
 plc = plc.value_counts()
 plt.figure(figsize=(10, 10))
 plt.pie(plc, labels=plc.index)
-plt.title("Доли ирисов по длине лепестка")
+plt.title("По длине лепестка")
 plt.show()
 
 
