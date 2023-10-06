0a15296cb301c702d477def139c500df17f7b971
katerinaivanova77 <katerina_ivanova.05@mail.ru>
2023-10-06 18:09:00 +0300

    solved contest

---
+++ gg.py
@@ -0,0 +1,21 @@
+
+
+nums = list(map(int, input().split()))
+def maxik(nums):
+    if len(nums) == 0:
+        return 0
+
+    if len(nums) == 1:
+        return nums[0]
+
+    prev_max = nums[0]
+    curr_max = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        temp = curr_max
+        curr_max = max(prev_max + nums[i], curr_max)
+        prev_max = temp
+
+    return curr_max
+result = maxik(nums)
+print(result)
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 iris_data = pd.read_csv("iris_data.csv")
 
 species_counts = iris_data["Species"].value_counts()
-plt.figure(figsize=(10, 10))
-plt.pie(species_counts, labels=species_counts.index)
+plt.figure(figsize=(6, 6))
+plt.pie(species_counts, labels=species_counts.index, autopct="%1.1f%%")
 plt.title("Доля видов ирисов в датасете")
 plt.show()
 
-plc = pd.cut(iris_data["PetalLengthCm"], bins=[0, 1.2, 1.5, np.inf], labels=["<= 1.2", "> 1.2 and <= 1.5", "> 1.5"])
-plc = plc.value_counts()
-plt.figure(figsize=(10, 10))
-plt.pie(plc, labels=plc.index)
+petal_length_bins = pd.cut(iris_data["PetalLengthCm"], bins=[0, 1.2, 1.5, np.inf], labels=["<= 1.2", "> 1.2 and <= 1.5", "> 1.5"])
+petal_length_counts = petal_length_bins.value_counts()
+plt.figure(figsize=(6, 6))
+plt.pie(petal_length_counts, labels=petal_length_counts.index, autopct="%1.1f%%")
 plt.title("Доли ирисов по длине лепестка")
 plt.show()
-
-
